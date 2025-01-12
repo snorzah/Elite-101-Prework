b3c0fa231d90dfc0633b3a51e7cdecf63ed5f5c8
snorzah <hamzahraoof@gmail.com>
2025-01-12 20:19:25 +0000

    Added user help query

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ print(f"Welcome {name} (age {age}) to Flower Child!")
 orderList = []
 orderDict = {"tomato" : 5.99, "chicken" : 10.99, "onion" : 3.99}
 total = 0
-while True:
-    print("The current Menu is: ")
-    for i in orderDict.keys():
-        print(i)
-    order = input("Please Enter Your Order (type \"done\" if finished): ")
-    if (order == "done"):
-        break
-    elif (order in orderDict):
-        total += orderDict[order]
-    elif (order not in orderDict):
-        print("Please Enter a Valid Menu Item")
-        continue
-    orderList.append(order)
-    print(f"You added {order} to your cart.")
-print("Your final order is: ")
-for item in orderList:
-    print(item)
-print(f"With a total price of {total}")
+age = input("Can I help you? ")
+if (age.lower() == "yes"):
+    while True:
+        print("The current Menu is: ")
+        for i in orderDict.keys():
+            print(i)
+        order = input("Please Enter Your Order (type \"done\" if finished): ")
+        if (order == "done"):
+            break
+        elif (order in orderDict):
+            total += orderDict[order]
+        elif (order not in orderDict):
+            print("Please Enter a Valid Menu Item")
+            continue
+        orderList.append(order)
+        print(f"You added {order} to your cart.")
+    print("Your final order is: ")
+    for item in orderList:
+        print(item)
+    print(f"With a total price of {total}")
+else:
+    print("Okay! Goodbye!")
