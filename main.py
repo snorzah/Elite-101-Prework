fad01cb441cf5fcba0224367ef530a82367aac90
snorzah <hamzahraoof@gmail.com>
2025-01-12 19:22:32 +0000

    Added prices

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ print("Hello, Welcome to the Flower Child Chatbot!")
 name = input("What is your name? ")
 print(f"Welcome {name} to Flower Child!")
 orderList = []
+orderDict = {"tomato" : 5.99, "chicken" : 10.99, "onion" : 3.99}
+total = 0
 while True:
+    print("The current Menu is: ")
+    for i in orderDict.keys():
+        print(i)
     order = input("Please Enter Your Order (type \"done\" if finished): ")
     if (order == "done"):
         break
+    elif (order in orderDict):
+        total += orderDict[order]
     orderList.append(order)
     print(f"You added {order} to your cart.")
 print("Your final order is: ")
 for item in orderList:
     print(item)
+print(f"With a total price of {total}")
